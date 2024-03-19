ab0805b9dd7bec9bcc83e46f8bf376c1e1765134
Nick Latcham <nick.latcham@proton.me>
2024-03-18 21:36:11 -0500

    ..

--- server/routes.go
+++ server/routes.go
@@ -29,80 +29,125 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux.HandleFunc("POST /", s.newTodoHandler)
 	mux.HandleFunc("PUT /{id}", s.updateHandler)
 	mux.HandleFunc("DELETE /{id}", s.deleteHandler)
+	mux.HandleFunc("DELETE /many", s.deleteManyHandler)
 
 	return mux
 }
 
 func (s *Server) rootHandler(rw http.ResponseWriter, req *http.Request) {
 	tasks, err := s.db.GetAllTodos(context.Background())
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.tmpl.ExecuteTemplate(rw, "base", tasks)
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 }
 
 func (s *Server) getAll(rw http.ResponseWriter, req *http.Request) {
 	data, err := s.db.GetAllTodos(context.Background())
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.tmpl.ExecuteTemplate(rw, "todo-list", data)
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 }
 
 func (s *Server) getAllActive(rw http.ResponseWriter, req *http.Request) {
 	data, err := s.db.GetAllActiveTodos(context.Background())
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.tmpl.ExecuteTemplate(rw, "todo-list", data)
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 }
 
 func (s *Server) getAllCompleted(rw http.ResponseWriter, req *http.Request) {
 	data, err := s.db.GetAllCompletedTodos(context.Background())
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.tmpl.ExecuteTemplate(rw, "todo-list", data)
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 }
 
 func (s *Server) updateHandler(rw http.ResponseWriter, req *http.Request) {
 	id := strings.TrimPrefix(req.URL.RequestURI(), "/")
 	val, err := strconv.Atoi(id)
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.db.ToggleCompleted(context.Background(), int32(val))
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 }
 
 func (s *Server) deleteHandler(rw http.ResponseWriter, req *http.Request) {
 	id := strings.TrimPrefix(req.URL.RequestURI(), "/")
+
 	val, err := strconv.Atoi(id)
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.db.DeleteOneTodo(context.Background(), int32(val))
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
+
+	rw.WriteHeader(200)
+}
+
+func (s *Server) deleteManyHandler(rw http.ResponseWriter, req *http.Request) {
+	data, err := s.db.GetAllActiveTodos(context.Background()) //optimistic response for speeeeeeed
+	if err != nil {
+		s.log.Error(err)
+	}
+
+	err = s.db.DeleteCompletedTodos(context.Background())
+	if err != nil {
+		s.log.Error(err)
+	}
+
+	err = s.tmpl.ExecuteTemplate(rw, "todo-list", data)
+	if err != nil {
+		s.log.Error(err)
+	}
 }
 
 func (s *Server) newTodoHandler(rw http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	b, err := strconv.ParseBool(req.Form.Get("completed"))
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	todo, err := s.db.CreateTodo(context.Background(), database.CreateTodoParams{
 		Task:      req.Form.Get("task"),
 		Completed: b,
 	})
-	s.LogError(err)
+	if err != nil {
+		s.log.Error(err)
+	}
 
 	err = s.tmpl.ExecuteTemplate(rw, "todo-item", todo)
-	s.LogError(err)
-}
-
-// LogError is a Server method that takes an error
-// and if the error is some, it will log it.
-func (s *Server) LogError(err error) {
 	if err != nil {
 		s.log.Error(err)
 	}
